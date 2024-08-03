0e0cca4b53951b22cc008a84903cbbd62777e713
Max46656 <117962384+Max46656@users.noreply.github.com>
2024-08-03 18:43:04 +0800

    Update getOutMyWay.js
    getOutMyWay
    功能調整：
    - 因應網域中不同頁面的需求細分，
    更新DomainStrategy為使用正則表達式以辨別網址，
    現在可以對同一個網站中的所有或部份頁面套用不同的規則。

--- 美觀/getOutMyWay/getOutMyWay.js
+++ 美觀/getOutMyWay/getOutMyWay.js
@@ -11,7 +11,7 @@
 // @description:de Entfernt störende Elemente und ermöglicht es Benutzern, ihre eigenen Elementselektoren hinzuzufügen (empfohlen für die Verwendung mit Skripten, die die Seitendarstellung anpassen)
 // @namespace   https://github.com/Max46656
 // @match       *://*/*
-// @version     1.0.0
+// @version     1.1.0
 // @author      Max
 // @icon        https://cdn-icons-png.flaticon.com/512/867/867787.png
 // @grant       GM_registerMenuCommand
@@ -38,41 +38,47 @@ class ElementHider {
 
 class DomainStrategy {
     constructor() {
-        this.domainSelectorsMap = {};
+        this.regexSelectorsMap = {};
     }
 
     async loadSelectors() {
-        this.domainSelectorsMap = await GM.getValue('domainSelectorsMap', {});
+        this.regexSelectorsMap = await GM.getValue('regexSelectorsMap', {});
     }
 
     async saveSelectors() {
-        await GM.setValue('domainSelectorsMap', this.domainSelectorsMap);
+        await GM.setValue('regexSelectorsMap', this.regexSelectorsMap);
     }
 
-    getSelectorsForDomain(domain) {
-        return this.domainSelectorsMap[domain] || [];
-    }
-
-    addSelectorToDomain(domain, selector) {
-        if (!this.domainSelectorsMap[domain]) {
-            this.domainSelectorsMap[domain] = [];
+    getSelectorsForUrl(url) {
+        for (const [regexStr, selectors] of Object.entries(this.regexSelectorsMap)) {
+            const regex = new RegExp(regexStr);
+            if (regex.test(url)) {
+                return selectors;
+            }
         }
-        this.domainSelectorsMap[domain].push(selector);
+        return [];
+    }
+
+    addSelectorToRegex(regexStr, selector) {
+        if (!this.regexSelectorsMap[regexStr]) {
+            this.regexSelectorsMap[regexStr] = [];
+        }
+        this.regexSelectorsMap[regexStr].push(selector);
         this.saveSelectors();
     }
 
-    removeSelectorFromDomain(domain, selector) {
-        if (this.domainSelectorsMap[domain]) {
-            this.domainSelectorsMap[domain] = this.domainSelectorsMap[domain].filter(item => item !== selector);
-            if (this.domainSelectorsMap[domain].length === 0) {
-                delete this.domainSelectorsMap[domain];
+    removeSelectorFromRegex(regexStr, selector) {
+        if (this.regexSelectorsMap[regexStr]) {
+            this.regexSelectorsMap[regexStr] = this.regexSelectorsMap[regexStr].filter(item => item !== selector);
+            if (this.regexSelectorsMap[regexStr].length === 0) {
+                delete this.regexSelectorsMap[regexStr];
             }
             this.saveSelectors();
         }
     }
 
-    getAllDomains() {
-        return Object.keys(this.domainSelectorsMap);
+    getAllRegexes() {
+        return Object.keys(this.regexSelectorsMap);
     }
 }
 
@@ -82,57 +88,103 @@ class MenuManager {
         this.initMenu();
     }
 
+    getMenuLabels() {
+        const userLang = navigator.language || navigator.userLanguage;
+        const labels = {
+            'zh-TW': {
+                viewAndAdd: '檢視並新增選擇器',
+                viewAndRemove: '檢視並刪除選擇器',
+                showAllRegexes: '顯示所有正則表達式',
+                enterRegex: '輸入正則表達式：',
+                currentSelectors: '當前選擇器：',
+                enterNewSelector: '輸入新的選擇器：',
+                enterSelectorToDelete: '輸入要刪除的選擇器：',
+                savedRegexes: '已儲存的正則表達式：',
+                enterRegexToView: '輸入要檢視的正則表達式：'
+            },
+            'en': {
+                viewAndAdd: 'View and Add Selectors',
+                viewAndRemove: 'View and Remove Selectors',
+                showAllRegexes: 'Show All Regexes',
+                enterRegex: 'Enter the regex:',
+                currentSelectors: 'Current selectors:',
+                enterNewSelector: 'Enter the new selector:',
+                enterSelectorToDelete: 'Enter the selector to delete:',
+                savedRegexes: 'Saved regexes:',
+                enterRegexToView: 'Enter the regex to view:'
+            },
+            'ja': {
+                viewAndAdd: 'セレクターを表示して追加',
+                viewAndRemove: 'セレクターを表示して削除',
+                showAllRegexes: 'すべての正則表現を表示',
+                enterRegex: '正則表現を入力してください：',
+                currentSelectors: '現在のセレクター：',
+                enterNewSelector: '新しいセレクターを入力してください：',
+                enterSelectorToDelete: '削除するセレクターを入力してください：',
+                savedRegexes: '保存された正則表現：',
+                enterRegexToView: '表示する正則表現を入力してください：'
+            }
+        };
+
+        // 回傳對應語言的選單文字，若使用者的語言不在支援列表中，回傳英文
+        return labels[userLang] || labels['en'];
+    }
+
     async initMenu() {
         await this.strategy.loadSelectors();
 
-        GM_registerMenuCommand('檢視並新增選擇器', this.viewAndAddSelectors.bind(this));
-        GM_registerMenuCommand('檢視並刪除選擇器', this.viewAndDeleteSelectors.bind(this));
-        GM_registerMenuCommand('顯示所有網域', this.showAllDomains.bind(this));
+        const labels = this.getMenuLabels();
+
+        GM_registerMenuCommand(labels.viewAndAdd, this.viewAndAddSelectors.bind(this));
+        GM_registerMenuCommand(labels.viewAndRemove, this.viewAndDeleteSelectors.bind(this));
+        GM_registerMenuCommand(labels.showAllRegexes, this.showAllRegexes.bind(this));
     }
 
     async viewAndAddSelectors() {
-        const domain = prompt('請輸入網域：', window.location.hostname);
-        if (domain) {
-            const currentSelectors = this.strategy.getSelectorsForDomain(domain);
-            alert(`當前選擇器：\n${currentSelectors.join('\n')}`);
-            const newSelector = prompt('請輸入要新增的選擇器：');
+        const labels = this.getMenuLabels();
+        const regexStr = prompt(labels.enterRegex, window.location.hostname + "/*");
+        if (regexStr) {
+            const currentSelectors = this.strategy.getSelectorsForUrl(regexStr);
+            alert(`${labels.currentSelectors}\n${currentSelectors.join('\n')}`);
+            const newSelector = prompt(labels.enterNewSelector);
             if (newSelector) {
-                this.strategy.addSelectorToDomain(domain, newSelector);
-                alert(`已新增選擇器：${newSelector}`);
+                this.strategy.addSelectorToRegex(regexStr, newSelector);
+                alert(`Added selector: ${newSelector}`);
             }
         }
     }
 
     async viewAndDeleteSelectors() {
-        const domain = prompt('請輸入網域：', window.location.hostname);
-        if (domain) {
-            const currentSelectors = this.strategy.getSelectorsForDomain(domain);
-            alert(`當前選擇器：\n${currentSelectors.join('\n')}`);
-            const selectorToDelete = prompt('請輸入要刪除的選擇器：');
+        const labels = this.getMenuLabels();
+        const regexStr = prompt(labels.enterRegex, window.location.hostname + "/*");
+        if (regexStr) {
+            const currentSelectors = this.strategy.getSelectorsForUrl(regexStr);
+            alert(`${labels.currentSelectors}\n${currentSelectors.join('\n')}`);
+            const selectorToDelete = prompt(labels.enterSelectorToDelete);
             if (selectorToDelete) {
-                this.strategy.removeSelectorFromDomain(domain, selectorToDelete);
-                alert(`已刪除選擇器：${selectorToDelete}`);
+                this.strategy.removeSelectorFromRegex(regexStr, selectorToDelete);
+                alert(`Deleted selector: ${selectorToDelete}`);
             }
         }
     }
 
-    async showAllDomains() {
-        const allDomains = this.strategy.getAllDomains();
-        const domain = prompt(`已儲存的網域：\n${allDomains.join('\n')}\n\n請輸入要檢視的網域：`);
-        if (domain) {
-            const selectors = this.strategy.getSelectorsForDomain(domain);
-            alert(`網域 ${domain} 的選擇器：\n${selectors.join('\n')}`);
+    async showAllRegexes() {
+        const labels = this.getMenuLabels();
+        const allRegexes = this.strategy.getAllRegexes();
+        const regexStr = prompt(`${labels.savedRegexes}\n${allRegexes.join('\n')}\n\n${labels.enterRegexToView}`);
+        if (regexStr) {
+            const selectors = this.strategy.getSelectorsForUrl(regexStr);
+            alert(`Selectors for regex ${regexStr}:\n${selectors.join('\n')}`);
         }
     }
 }
-
 
 async function main() {
     const strategy = new DomainStrategy();
     await strategy.loadSelectors();
 
-    const currentDomain = window.location.hostname;
-    const selectors = strategy.getSelectorsForDomain(currentDomain);
+    const currentUrl = window.location.href;
+    const selectors = strategy.getSelectorsForUrl(currentUrl);
 
     if (selectors.length > 0) {
         const hider = new ElementHider(selectors);
