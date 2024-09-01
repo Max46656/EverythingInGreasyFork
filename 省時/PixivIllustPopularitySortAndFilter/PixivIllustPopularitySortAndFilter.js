1c5677d653941554e754a9cf410e0a69a1b7fa83
Max46656 <117962384+Max46656@users.noreply.github.com>
2024-09-01 21:13:54 +0800

    Update PixivIllustPopularitySortAndFilter.js
    功能調整：
    - 因應不支援的頁面會跳錯誤導致程式停止運作。
    更新全域observer呼叫artScraper實例addStartButton()的錯誤參數；
    更新artScraper.setStrategy()，從throw new error改為console.error。
    現在從不支援頁面跳至支援頁面時會即刻彈出按鈕。

--- 省時/PixivIllustPopularitySortAndFilter/PixivIllustPopularitySortAndFilter.js
+++ 省時/PixivIllustPopularitySortAndFilter/PixivIllustPopularitySortAndFilter.js
@@ -7,7 +7,7 @@
 // @description:ja  フォローアーティスト作品、アーティスト作品、タグ作品ページで、いいね數でソートし、閾値以上の作品のみを表示します。
 // @description:en  Sort Illustration by likes and display only those above the threshold on followed artist illustrations, artist illustrations, and tag illustrations pages.
 // @namespace    https://github.com/Max46656
-// @version      1.7.5
+// @version      1.7.6
 // @author       Max
 // @match        https://www.pixiv.net/bookmark_new_illust.php*
 // @match        https://www.pixiv.net/users/*
@@ -128,7 +128,7 @@ class artScraper {
         } else if (url.match(/^https:\/\/www\.pixiv\.net\/(en\/tags|tags)\/.*\/.*$/)) {
             return new tagsStrategy();
         } else {
-            throw new Error('Unsupported page type');
+            console.error('Unsupported page type');
         }
     }
 
@@ -567,7 +567,7 @@ class artScraper {
         let max = await this.getMaxPage();
         const stepSize = Math.floor(max / 24);
 
-        console.log(this.getMaxPage());
+        //console.log(this.getMaxPage());
         pageRangeInput.max = max || 34;
 
         if (this.targetPages > max) {
@@ -747,8 +747,8 @@ const observer = new MutationObserver(function(mutations) {
             return;
         }
         pageUrl = window.location.href;
-        johnTheHornyOne = new artScraper(10, 50);
-        johnTheHornyOne.addStartButton(johnTheHornyOne.strategy.getButtonAtClass(), johnTheHornyOne.strategy.getAllButtonClass()[0]);
+        let johnTheHornyOne = new artScraper(10, 50);
+        johnTheHornyOne.addStartButton(johnTheHornyOne.strategy.getButtonAtClass(), johnTheHornyOne.strategy.getAllButtonClass());
     });
 });
 let config = {childList: true,};
