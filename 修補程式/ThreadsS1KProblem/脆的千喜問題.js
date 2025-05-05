8f400f4ca6fdbc5ae252d2b6682fa371ae06e819
Max46656 <117962384+Max46656@users.noreply.github.com>
2025-05-05 14:28:52 +0800

    Update 脆的千喜問題.js
    Debug：
    - 因應網域更新；增加match；現在能夠識別更新後的網域。
    功能調整：
    - 新增GM.info；增加控制台資訊；現在能夠識別提示來自哪個腳本。

--- 修補程式/ThreadsS1KProblem/脆的千喜問題.js
+++ 修補程式/ThreadsS1KProblem/脆的千喜問題.js
@@ -4,13 +4,14 @@
 // @name:en     ThreadsS1KProblem
 // @description  刪除Threads的互動統計數據中超過1K時會出現的".0"，其應是整數而非浮點數。
 // @namespace    https://github.com/Max46656
-// @version      1.2
+// @version      1.2.1
 // @author       Max
 // @description:ja Threads のインタラクション統計データで、1K を超える場合に表示される「.0」を削除し、整數にする必要があります。
 // @description:en Delete ".0" in Threads interact stat when it's over 1K, which should be an integer instead of a float.
+// @match        https://www.threads.com/*
 // @match        https://www.threads.net/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=threads.net
-// @grant        none
+// @grant        GM.info
 // @license MPL2.0
 // @downloadURL https://update.greasyfork.org/scripts/496055/%E8%84%86%E7%9A%84%E5%8D%83%E5%96%9C%E5%95%8F%E9%A1%8C.user.js
 // @updateURL https://update.greasyfork.org/scripts/496055/%E8%84%86%E7%9A%84%E5%8D%83%E5%96%9C%E5%95%8F%E9%A1%8C.meta.js
@@ -21,7 +22,7 @@ class InteractStatChanger {
     constructor() {
         this.interactStatObserver = new MutationObserver(this.handleAllPosts.bind(this));
         // 空格以搜尋符合條件的父元素下符合條件的子元素，將class中的空格以"."取代
-        this.interactStatClassName='div.x6ikm8r.x10wlt62 span.x1lliihq.x1plvlek.xryxfnj.x1n2onr6.x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x1i0vuye.xmd891q.xo1l8bm';
+        this.interactStatClassName='span.x17qophe.x10l6tqk.x13vifvy';
     }
 
     startObserving() {
@@ -36,7 +37,7 @@ class InteractStatChanger {
         this.stopObserving();
 
         const interactStatElements = document.querySelectorAll(this.interactStatClassName);
-        console.log('interactStat有', interactStatElements.length, '個 at ', new Date().toLocaleString());
+        //console.log('interactStat有', interactStatElements.length, '個 at ', new Date().toLocaleString());
         interactStatElements.forEach(element => {
             Array.from(element.childNodes).forEach(node => {
                 node.nodeValue = this.toInteger(node);
@@ -50,7 +51,7 @@ class InteractStatChanger {
         if (node.nodeType !== Node.TEXT_NODE || !node.nodeValue.match(/\.0/g)) {
             return node.nodeValue;
         } else {
-            console.log('L1KProblemFound ', node.nodeValue);
+            console.log(`${GM_info.script.name} L1KProblemFound`, node.nodeValue);
             return node.nodeValue.replace(/\.0/, '');
         }
     }
