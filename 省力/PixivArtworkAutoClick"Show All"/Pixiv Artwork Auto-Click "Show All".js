96d4285775bff40e62de4441e067a815e530f827
Max46656 <117962384+Max46656@users.noreply.github.com>
2025-07-22 13:19:11 +0800

    Update Pixiv Artwork Auto-Click "Show All".js
    1.0.4
    功能調整：
    - 因應Ajax跳轉不會觸發程式；
    新增ReadingStand.setupUrlChangeListener()；
    現在在作品頁面點選其他作品時也能正確觸發程式。
    
    重構：
    - 因應新增setupUrlChangeListener()；
    將load觸發整合進該方法中。

--- 省力/PixivArtworkAutoClick"Show All"/Pixiv Artwork Auto-Click "Show All".js
+++ 省力/PixivArtworkAutoClick"Show All"/Pixiv Artwork Auto-Click "Show All".js
@@ -1,19 +1,17 @@
 // ==UserScript==
-// @name        Pixiv自動點選「檢視全部」
-// @name:ja     Pixiv「すべて見る」を自動クリック
-// @name:en     Pixiv Auto-Click "View All"
-// @description 當Pixiv作品或頁面包含多張圖片時，自動展開(漫畫作品不受影響)
-// @description:en Automatically expands Pixiv artworks or pages with multiple images (manga artworks are unaffected)
-// @description:ja Pixivの作品またはページに複数の画像が含まれる場合、自動的に展開します（漫画作品には影響しません）
+// @name        Pixiv作品自動點選「檢視全部」
+// @name:ja     Pixiv作品「すべて見る」を自動クリック
+// @name:en     Pixiv Artwork Auto-Click "Show All"
+// @description 當Pixiv作品包含多張圖片時，自動展開，漫畫作品不受影響
+// @description:en Automatically expands Pixiv artworks with multiple images, leaving manga artworks unaffected
+// @description:ja Pixivの複數畫像の作品を自動的に展開し、漫畫作品には影響を與えない
 
 // @match       https://www.pixiv.net/artworks/*
 // @match       https://www.pixiv.net/en/artworks/*
 // @match       https://www.pixiv.net/users/*
 // @match       https://www.pixiv.net/en/artworks/*
-// @match       https://www.pixiv.net/tags/*
-// @match       https://www.pixiv.net/en/tags/*
 // @grant       none
-// @version     1.0.3
+// @version     1.0.4
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=pixiv.net
 
 // @author      Max
@@ -24,30 +22,66 @@
 // ==/UserScript==
 
 class ReadingStand {
-    static expandArtwork() {
-        const notMangaTexts = ['檢視全部', 'すべて見る', 'Show all', '모두 보기'];
+    constructor() {
+        this.notMangaTexts = ['查看全部', '檢視全部', 'すべて見る', 'Show all', '모두 보기'];
+        this.setupUrlChangeListener();
+    }
 
+    expandArtwork() {
         const viewAllButton = Array.from(document.querySelectorAll('button:not(:disabled)'))
             .find(btn =>
                 window.getComputedStyle(btn).display !== 'none' &&
-                notMangaTexts.some(text => btn.textContent.includes(text))
-                );
-
+                this.notMangaTexts.some(text => btn.textContent.includes(text))
+            );
         if (viewAllButton) {
             viewAllButton.click();
         }
     }
-  static expandGallery() {
+
+    expandGallery() {
         const artistHomePattern = /^https:\/\/www\.pixiv\.net\/(en\/users|users)\/[0-9]*$/;
         const tagHomePattern = /^.*:\/\/www\.pixiv\.net\/(en\/tags|tags)\/.*$/;
         const tagPagePattern = /^.*:\/\/www\.pixiv\.net\/(en\/tags|tags)\/.*\/artworks*/;
-        if (artistHomePattern.test(self.location.href) || !tagPagePattern.test(self.location.href) && tagHomePattern.test(self.location.href)) {
+        if (artistHomePattern.test(self.location.href) || (!tagPagePattern.test(self.location.href) && tagHomePattern.test(self.location.href))) {
             self.location.href = self.location.href + "/artworks?p=1";
         }
     }
+
+    setupUrlChangeListener() {
+        const delay = (n) => new Promise(r => setTimeout(r, n * 1000));
+
+        const oldPushState = history.pushState;
+        history.pushState = function pushState() {
+            const result = oldPushState.apply(this, arguments);
+            window.dispatchEvent(new Event('pushstate'));
+            window.dispatchEvent(new Event('locationchange'));
+            return result;
+        };
+
+        const oldReplaceState = history.replaceState;
+        history.replaceState = function replaceState() {
+            const result = oldReplaceState.apply(this, arguments);
+            window.dispatchEvent(new Event('replacestate'));
+            window.dispatchEvent(new Event('locationchange'));
+            return result;
+        };
+
+        window.addEventListener('popstate', () => {
+            window.dispatchEvent(new Event('locationchange'));
+        });
+
+
+        window.addEventListener("load", () => {
+            window.dispatchEvent(new Event('locationchange'));
+        });
+
+        window.addEventListener('locationchange', () => {
+            delay(0.7).then(() => {
+                this.expandArtwork();
+                this.expandGallery();
+            });
+        });
+    }
 }
 
-window.addEventListener("load", () => {
-    ReadingStand.expandArtwork();
-    ReadingStand.expandGallery();
-});
+const readingStand = new ReadingStand();
