97a87d2680a1794340efbd5fafcb5c9a90d39d64
Max46656 <117962384+Max46656@users.noreply.github.com>
2025-07-26 20:47:19 +0800

    Update PixivIllustPopularitySortAndFilter.js
    1.9.4
    Debug：
    - 因應Pixiv更改元素名稱；
    修改tag策略的按鈕渲染類；
    現在tag頁面也能夠看到啟動按鈕了。

--- 省時/PixivIllustPopularitySortAndFilter/PixivIllustPopularitySortAndFilter.js
+++ 省時/PixivIllustPopularitySortAndFilter/PixivIllustPopularitySortAndFilter.js
@@ -7,7 +7,7 @@
 // @description:ja  フォローアーティスト作品、アーティスト作品、タグ作品ページで、いいね數でソートし、閾値以上の作品のみを表示します。
 // @description:en  Sort Illustration by likes and display only those above the threshold on followed artist illustrations, artist illustrations, and tag illustrations pages.
 // @namespace    https://github.com/Max46656
-// @version      1.9.3
+// @version      1.9.4
 // @author       Max
 // @match        https://www.pixiv.net/bookmark_new_illust.php*
 // @match        https://www.pixiv.net/users/*
@@ -74,7 +74,7 @@ class tagsStrategy extends pageStrategy{
         return 'gqvfWY';
     }
     getButtonAtClass() {
-        return 'section.sc-3d8ed48f-0 div.sc-8d5ac044-4 div.sc-a6755c3a-0';
+        return 'section.sc-498c1dba-0 div.sc-8d5ac044-4 div.sc-a6755c3a-0';
     }
     getAllButtonClass() {
         return ['bpnJkX','dPRDcU','eXDUmY','eXsdAL','bbWWKH','bbWEWN','fjcIyQ'];
