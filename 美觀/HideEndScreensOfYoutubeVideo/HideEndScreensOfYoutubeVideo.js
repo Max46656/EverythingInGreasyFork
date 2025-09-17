498e9110e529494d538d06723b0a1eaa72a750ac
Max <117962384+Max46656@users.noreply.github.com>
2025-09-17 17:15:16 +0800

    BREAKING CHANGE(HideEndScreensOfYoutubeVideo): 將腳本改寫為純CSS

--- 美觀/HideEndScreensOfYoutubeVideo/HideEndScreensOfYoutubeVideo.js
+++ 美觀/HideEndScreensOfYoutubeVideo/HideEndScreensOfYoutubeVideo.js
@@ -1,78 +1,36 @@
-// ==UserScript==
-// @name         隱藏youtube片尾畫面
-// @name:ja      YouTubeの終了畫面を非表示にする。
-// @name:en      Hide end-screens of YouTube.
-// @namespace    https://github.com/Max46656
-// @version      1.0
-// @description  自動隱藏 YouTube 影片結尾
-// @description:en Automatically hide end-screens at the end of YouTube videos.
-// @description:ja YouTube動畫の最後にある終了畫面を自動的に非表示にします。
-// @author       Max
-// @license      MIT
-// @match        https://www.youtube.com/
-// @match        https://www.youtube.com/watch?v=*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @grant        none
-// ==/UserScript==
-
-class youTubeRecommendationHider {
-    constructor(recommendedVideosClass,maxRecommend,maxCheckCount,videoSrc = null,recommendedVideosCount=0) {
-        this.recommendedVideosClass = recommendedVideosClass;
-        this.maxRecommend = maxRecommend;
-        this.maxCheckCount = maxCheckCount;
-        this.maxCheckCountReseter=maxCheckCount;
-        this.videoSrc = videoSrc;
-        this.recommendedVideosCount = recommendedVideosCount;
-        this.videoPlayer = document.querySelector('video');
+/* ==UserStyle==
+@name         乾淨的YouTube 影片結尾
+@name:en      Clean YouTube Video Ending
+@name:ja      クリーンなYouTube動畫の終了
+@name:de      Sauberes YouTube-Videoende
+@name:uk      Чисте завершення відео YouTube
+@description  把其他推薦影片、創作者資訊與更多有的沒的改成透明，直到你將滑鼠指向該區域
+@description:en Makes other recommended videos, creator info, and other elements transparent until you hover over the area
+@description:ja 他の推薦動畫、クリエイター情報、その他の要素をマウスをホバーするまで透明にします
+@description:de Macht andere empfohlene Videos, Erstellerinformationen und andere Elemente transparent, bis Sie mit der Maus darüberfahren
+@description:uk Робить інші рекомендовані відео, інформацію про автора та інші елементи прозорими, доки ви не наведете на них курсор
+ 
+@author       Max
+@namespace    https://github.com/Max46656
+@supportURL   https://github.com/Max46656/EverythingInGreasyFork/issues
+ 
+@version      2.0.0
+@license      MPL2.0
+@preprocessor default
+==/UserStyle== */
+ 
+@-moz-document url-prefix("https://www.youtube.com/") {
+    :root {
+        --default-opacity: 0;   /* 預設透明度 */
+        --hover-opacity: 0.8;   /* 滑鼠懸停時的透明度 */
     }
-
-    hideRecommendVideos() {
-        let allRecommendedViedo = document.querySelectorAll(this.recommendedVideosClass);
-        allRecommendedViedo.forEach(element => {
-            console.log('刪除' + element.querySelector('a').href);
-            element.remove();
-            this.recommendedVideosCount++;
-            console.log('刪除推薦影片數' + this.recommendedVideosCount);
-        });
-        if (this.recommendedVideosCount < this.maxRecommend && this.maxCheckCount > 0) {
-            this.maxCheckCount--;
-            console.log('剩餘檢查次數' + this.maxCheckCount);
-            setTimeout(() => this.hideRecommendVideos(), 1000);
-        }
+ 
+    ytd-reel-player-overlay-renderer .metadata-container.style-scope {
+        opacity: var(--default-opacity) !important;
+        transition: opacity 0.3s ease !important;
     }
-
-    resetVariables() {
-        console.log('重置變數');
-        this.recommendedVideosCount = 0;
-        this.maxCheckCount = this.maxCheckCountReseter;
-        this.hideRecommendVideos();
-    }
-
-    newTab() {
-        if (this.videoSrc == null) {
-            console.log('新YouTube分頁');
-            this.hideRecommendVideos();
-        }
-    }
-
-    sameTab() {
-        if (this.videoPlayer) {
-            this.videoSrc = this.videoPlayer.src;
-            console.log('影片偵測<video>' + this.videoSrc);
-            const observer = new MutationObserver(mutations => {
-                mutations.forEach(mutation => {
-                    if (mutation.attributeName === 'src' && this.videoPlayer.src !== this.videoSrc) {
-                        console.log('新影片偵測' + this.videoPlayer.src);
-                        this.videoSrc = this.videoPlayer.src;
-                        this.resetVariables();
-                    }
-                });
-            });
-            observer.observe(this.videoPlayer, { attributes: true });
-        }
+ 
+    ytd-reel-player-overlay-renderer .metadata-container.style-scope:hover {
+        opacity: var(--hover-opacity) !important;
     }
 }
-
-const johnTheCleaner=new youTubeRecommendationHider('.ytp-ce-element',4,3);
-johnTheCleaner.newTab();
-johnTheCleaner.sameTab();
