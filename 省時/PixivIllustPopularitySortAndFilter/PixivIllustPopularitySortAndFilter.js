a1be3497221655d02ba2ecb6d954bc3cd357d648
Max <117962384+Max46656@users.noreply.github.com>
2026-01-16 03:34:09 +0800

    Feat(Pixiv作品熱門程度排序與篩選器): 改善因網站更新失效的元素選擇器（/users）

--- 省時/PixivIllustPopularitySortAndFilter/PixivIllustPopularitySortAndFilter.js
+++ 省時/PixivIllustPopularitySortAndFilter/PixivIllustPopularitySortAndFilter.js
@@ -7,7 +7,7 @@
 // @description:ja  フォローアーティスト作品、アーティスト作品、タグ作品ページで、いいね數でソートし、閾値以上の作品のみを表示します。
 // @description:en  Sort Illustration by likes and display only those above the threshold on followed artist illustrations, artist illustrations, and tag illustrations pages.
 // @namespace    https://github.com/Max46656
-// @version      1.10.1
+// @version      1.10.2
 // @author       Max
 // @match        https://www.pixiv.net/bookmark_new_illust.php*
 // @match        https://www.pixiv.net/users/*
@@ -46,7 +46,7 @@ class userStrategy extends pageStrategy{
         return 'div:not([class]) div div div:not([class]) div:not([class]) div:has(li[offset])';
     }
     getArtWallAlignLeftClass(){
-        return 'gqvfWY';
+        return 'iJEVBL';
     }
     getButtonAtClass() {
         return 'nav:not(:has(button))';
