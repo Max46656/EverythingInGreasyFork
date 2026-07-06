f6dc406c4e82e9e8d60e50d6c1c0a923eb6bf867
Max <117962384+Max46656@users.noreply.github.com>
2026-07-06 16:00:52 +0800

    Fix(最愛「新作品」更新): 更新pawchive網域

--- 省時/FavoritesNewArtUpdateOfKemono/FavoritesNewArtUpdateOfKemono.js
+++ 省時/FavoritesNewArtUpdateOfKemono/FavoritesNewArtUpdateOfKemono.js
@@ -11,10 +11,10 @@
 // @supportURL   https://github.com/Max46656/EverythingInGreasyFork/issues/new?template=bug_report.yml&labels=bug,userscript&title=%5B%E6%9C%80%E6%84%9B%E3%80%8C%E6%96%B0%E4%BD%9C%E5%93%81%E3%80%8D%E6%9B%B4%E6%96%B0%5D%20%E5%95%8F%E9%A1%8C%E5%9B%9E%E5%A0%B1
 // @license      MPL2.0
 //
-// @version      2.2.0
+// @version      2.2.2
 // @match        *://kemono.cr/*
 // @match        *://coomer.st/*
-// @match        *://pawchive.st/*
+// @match        *://pawchive.tld/*
 // @require      https://update.greasyfork.org/scripts/569411/1824218/SPA%20%E5%8B%95%E6%85%8B%E8%B7%AF%E7%94%B1%E7%9B%A3%E8%81%BD%E5%99%A8.js
 // @grant        none
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kemono.cr
@@ -76,8 +76,8 @@ class ArtistUpdateCatcher {
             creatorPostsApi ='https://kemono.cr/api/v1' + cleanUrl + '/posts';
             creatorInfoApi = 'https://kemono.cr/api/v1' + cleanUrl + '/profile';
         }else if(isPawchive){
-            creatorPostsApi ='https://pawchive.st/api/v1' + cleanUrl;
-            creatorInfoApi = 'https://pawchive.st/api/v1' + cleanUrl + '/profile';
+            creatorPostsApi ='https://pawchive.pw/api/v1' + cleanUrl;
+            creatorInfoApi = 'https://pawchive.pw/api/v1' + cleanUrl + '/profile';
         }else{
             creatorPostsApi ='https://coomer.st/api/v1' + cleanUrl + '/posts';
             creatorInfoApi = 'https://coomer.st/api/v1' + cleanUrl + '/profile';
@@ -93,7 +93,6 @@ class ArtistUpdateCatcher {
                 return this.fetchUpdateArticles(url);
             }
             const posts = await postsResponse.json();
-            //console.log(posts)
             if (posts.length === 0) {
                 return articles;
             }
@@ -125,7 +124,7 @@ class ArtistUpdateCatcher {
                 const attachmentsCount = post.attachments ? post.attachments.length : 0;
 
                 const href = `/${service}/user/${user}/post/${articleId}`;
-                const imgSrc = isPawchive ? `https://img.pawchive.st/thumbnail/data/${filePath}` : `${filePath}`;
+                const imgSrc = isPawchive ? `https://img.pawchive.pw/thumbnail/data/${filePath}` : `${filePath}`;
 
                 const articleHtml = `
                 <article class="post-card post-card--preview" data-id=${articleId} data-service=${service} data-user=${user} style="position: relative; overflow: hidden; border-radius: 2%;font-size: larger;">
@@ -155,8 +154,6 @@ class ArtistUpdateCatcher {
                 const doc = parser.parseFromString(articleHtml, 'text/html');
                 const articleElement = doc.body.firstChild;
                 articles.push(articleElement);
-                      console.log(articleHtml)
-
             }
         } catch (error) {
             console.error(`獲取作品 ${url} 失敗:`, error);
